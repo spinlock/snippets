f893ef90a15a4e52a3378dcfc6ce9a664ddcdc09
spinlock <wnzheng@gmail.com>
2015-04-29 00:03:11 +0800

    strings, update

--- template/strings/strings.go
+++ template/strings/strings.go
@@ -3,7 +3,7 @@ package strings
 import "math"
 
 func StrStr(s, sep string) int {
-	i, j := 0, 0
+	var i, j int
 	for {
 		if j == len(sep) {
 			return i
@@ -29,7 +29,7 @@ func KmpStr(s, sep string) int {
 			prefix[j] = l + 1
 		}
 	}
-	i, j := 0, 0
+	var i, j int
 	for {
 		if j == len(sep) {
 			return i - j
@@ -47,42 +47,42 @@ func KmpStr(s, sep string) int {
 }
 
 func Atoi32(s string) int32 {
-	var idx = 0
+	var idx int
 	for idx < len(s) && s[idx] == ' ' {
 		idx++
 	}
 	if idx == len(s) {
 		return 0
 	}
+
 	var minus = false
 	switch s[idx] {
 	case '-':
-		minus = true
 		idx++
+		minus = true
 	case '+':
 		idx++
 	}
+
 	var out int32
-	for idx < len(s) {
-		switch b := s[idx]; {
-		case b >= '0' && b <= '9':
-			var old = out
+	for ; idx < len(s); idx++ {
+		if b := s[idx]; b >= '0' && b <= '9' {
+			tmp := 10 * out
 			if minus {
-				out = out*10 - int32(b-'0')
+				tmp -= int32(b - '0')
 			} else {
-				out = out*10 + int32(b-'0')
+				tmp += int32(b - '0')
 			}
-			if out/10 != old {
-				if minus {
-					return math.MinInt32
-				} else {
-					return math.MaxInt32
-				}
+			if tmp/10 == out {
+				out = tmp
+			} else if minus {
+				return math.MinInt32
+			} else {
+				return math.MaxInt32
 			}
-		default:
+		} else {
 			return out
 		}
-		idx++
 	}
 	return out
 }
