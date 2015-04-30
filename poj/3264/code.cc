45f46e82f4bd3d2882d1ab6dfce8d629c5898b0a
spinlock <wnzheng@gmail.com>
2015-04-30 19:25:38 +0800

    poj{3264}, update

--- poj/3264/code.cc
+++ poj/3264/code.cc
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 
 typedef struct {
     int beg, end;
     int min, max;
 } node_t;
+
+typedef struct {
+    node_t *nodes;
+} tree_t;
 
 int
 min_int(int v1, int v2) {
@@ -27,84 +30,96 @@ max_int(int v1, int v2) {
 }
 
 void
-intree_init_walk(node_t *t, int x, int *values, int beg, int end) {
-    t[x].beg = beg;
-    t[x].end = end;
+tree_init_walk(tree_t *t, int p, int *values, int beg, int end) {
+    node_t *x = &t->nodes[p];
+    x->beg = beg;
+    x->end = end;
     if (beg == end) {
-        t[x].min = values[beg];
-        t[x].max = values[beg];
+        x->min = values[beg];
+        x->max = values[end];
     } else {
-        int mid = (beg + end) / 2;
-        int l = 2 * x + 1;
-        int r = 2 * x + 2;
-        intree_init_walk(t, l, values, beg, mid);
-        intree_init_walk(t, r, values, mid + 1, end);
-        t[x].min = min_int(t[l].min, t[r].min);
-        t[x].max = max_int(t[l].max, t[r].max);
+        int mid = beg + (end - beg) / 2;
+        int l = p * 2 + 1;
+        int r = p * 2 + 2;
+        tree_init_walk(t, l, values, beg, mid);
+        tree_init_walk(t, r, values, mid + 1, end);
+        x->min = min_int(t->nodes[l].min, t->nodes[r].min);
+        x->max = max_int(t->nodes[l].max, t->nodes[r].max);
     }
-}
-
-node_t *
-intree_init(int *values, int n) {
-    node_t *t = (node_t *)malloc(sizeof(node_t) * (4 * n + 1));
-    if (n != 0) {
-        intree_init_walk(t, 0, values, 0, n - 1);
-    } else {
-        t[0].end = t[0].beg - 1;
-    }
-    return t;
 }
 
 void
-intree_minmax_walk(node_t *t, int x, int beg, int end, int *pmin, int *pmax) {
-    if (beg == t[x].beg && end == t[x].end) {
-        *pmin = min_int(*pmin, t[x].min);
-        *pmax = max_int(*pmax, t[x].max);
+tree_init(tree_t *t, int *values, int n) {
+    int max = 4 * n + 1;
+    t->nodes = (node_t *)malloc(sizeof(node_t) * max);
+    memset(t->nodes, 0, sizeof(node_t) * max);
+    if (n != 0) {
+        tree_init_walk(t, 0, values, 0, n - 1);
+    }
+}
+
+void
+tree_free(tree_t *t) {
+    free(t->nodes);
+}
+
+void
+tree_minmax_walk(tree_t *t, int p, int beg, int end, int *pmin, int *pmax) {
+    node_t *x = &t->nodes[p];
+    int min, max;
+    if (x->beg == beg && x->end == end) {
+        min = x->min;
+        max = x->max;
     } else {
-        int mid = (t[x].beg + t[x].end) / 2;
-        int l = 2 * x + 1;
-        int r = 2 * x + 2;
-        if (mid >= end) {
-            intree_minmax_walk(t, l, beg, end, pmin, pmax);
-        } else if (beg >= mid + 1) {
-            intree_minmax_walk(t, r, beg, end, pmin, pmax);
+        int mid = x->beg + (x->end - x->beg) / 2;
+        int l = p * 2 + 1;
+        int r = p * 2 + 2;
+        if (end <= mid) {
+            tree_minmax_walk(t, l, beg, end, &min, &max);
+        } else if (beg > mid) {
+            tree_minmax_walk(t, r, beg, end, &min, &max);
         } else {
-            intree_minmax_walk(t, l, beg, mid, pmin, pmax);
-            intree_minmax_walk(t, r, mid + 1, end, pmin, pmax);
+            int minl, maxl;
+            tree_minmax_walk(t, l, beg, mid, &minl, &maxl);
+            int minr, maxr;
+            tree_minmax_walk(t, r, mid + 1, end, &minr, &maxr);
+            min = min_int(minl, minr);
+            max = max_int(maxl, maxr);
         }
     }
+    *pmin = min;
+    *pmax = max;
 }
 
-void
-intree_minmax(node_t *t, int beg, int end, int *pmin, int *pmax) {
-    beg = max_int(beg, t[0].beg);
-    end = min_int(end, t[0].end);
+int
+tree_delta(tree_t *t, int beg, int end) {
+    beg = max_int(beg, t->nodes[0].beg);
+    end = min_int(end, t->nodes[0].end);
     if (beg > end) {
-        *pmin = *pmax = 0;
-    } else {
-        *pmin = INT_MAX, *pmax = INT_MIN;
-        intree_minmax_walk(t, 0, beg, end, pmin, pmax);
+        return 0;
     }
+    int min, max;
+    tree_minmax_walk(t, 0, beg, end, &min, &max);
+    return max - min;
 }
 
 int
 main(void) {
-    int n, q;
-    scanf("%d %d", &n, &q);
-    int *height = (int *)malloc(sizeof(int) * (n + 1));
-    height[0] = 0;
-    for (int i = 1; i <= n; i ++) {
-        scanf("%d", &height[i]);
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int *values = (int *)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i ++) {
+        scanf("%d", &values[i]);
     }
-    node_t *tree = intree_init(height, n + 1);
-    for (int i = 0; i < q; i ++) {
+
+    tree_t __t, *t = &__t;
+    tree_init(t, values, n);
+    for (int i = 0; i < m; i ++) {
         int beg, end;
-        int min, max;
         scanf("%d %d", &beg, &end);
-        intree_minmax(tree, beg, end, &min, &max);
-        printf("%d\n", max - min);
+        printf("%d\n", tree_delta(t, beg - 1, end - 1));
     }
-    free(tree);
-    free(height);
+    tree_free(t);
     return 0;
 }
--- poj/3264/main.go
+++ poj/3264/main.go
@@ -23,74 +23,80 @@ type node struct {
 	min, max int
 }
 
-type InTree []node
+type Tree struct {
+	nodes []node
+}
 
-func NewTree(values []int) InTree {
-	var t InTree = make([]node, len(values)*4+1)
+func New(values []int) *Tree {
+	t := &Tree{}
+	t.nodes = make([]node, len(values)*4+1)
 	if len(values) != 0 {
-		t.init(0, values, 0, len(values)-1)
-	} else {
-		t[0].end = t[0].beg - 1
+		t.init(values, 0, 0, len(values)-1)
 	}
 	return t
 }
 
-func (t InTree) init(x int, values []int, beg, end int) {
-	t[x].beg = beg
-	t[x].end = end
+func (t *Tree) init(values []int, p int, beg, end int) {
+	x := &t.nodes[p]
+	x.beg = beg
+	x.end = end
 	if beg == end {
-		t[x].min = values[beg]
-		t[x].max = values[beg]
+		x.min = values[beg]
+		x.max = values[beg]
 	} else {
-		mid := (beg + end) / 2
-		l := 2*x + 1
-		r := 2*x + 2
-		t.init(l, values, beg, mid)
-		t.init(r, values, mid+1, end)
-		t[x].min = minInt(t[l].min, t[r].min)
-		t[x].max = maxInt(t[l].max, t[r].max)
+		mid := beg + (end-beg)/2
+		l := p*2 + 1
+		r := p*2 + 2
+		t.init(values, l, beg, mid)
+		t.init(values, r, mid+1, end)
+		x.min = minInt(t.nodes[l].min, t.nodes[r].min)
+		x.max = maxInt(t.nodes[l].max, t.nodes[r].max)
 	}
 }
 
-func (t InTree) MinMax(beg, end int) (int, int) {
-	beg = maxInt(beg, t[0].beg)
-	end = minInt(end, t[0].end)
+func (t *Tree) Delta(beg, end int) int {
+	beg = maxInt(beg, t.nodes[0].beg)
+	end = minInt(end, t.nodes[0].end)
 	if beg > end {
-		return 0, 0
+		return 0
 	}
-	return t.minmax(0, beg, end)
+	min, max := t.minmax(0, beg, end)
+	return max - min
 }
 
-func (t InTree) minmax(x int, beg, end int) (int, int) {
-	if beg == t[x].beg && end == t[x].end {
-		return t[x].min, t[x].max
-	}
-	mid := (t[x].beg + t[x].end) / 2
-	l := 2*x + 1
-	r := 2*x + 2
-	if mid >= end {
-		return t.minmax(l, beg, end)
-	} else if beg >= mid+1 {
-		return t.minmax(r, beg, end)
+func (t *Tree) minmax(p int, beg, end int) (int, int) {
+	x := &t.nodes[p]
+	if x.beg == beg && x.end == end {
+		return x.min, x.max
 	} else {
-		minl, maxl := t.minmax(l, beg, mid)
-		minr, maxr := t.minmax(r, mid+1, end)
-		return minInt(minl, minr), maxInt(maxl, maxr)
+		mid := x.beg + (x.end-x.beg)/2
+		l := p*2 + 1
+		r := p*2 + 2
+		if end <= mid {
+			return t.minmax(l, beg, end)
+		} else if beg > mid {
+			return t.minmax(r, beg, end)
+		} else {
+			minl, maxl := t.minmax(l, beg, mid)
+			minr, maxr := t.minmax(r, mid+1, end)
+			return minInt(minl, minr), maxInt(maxl, maxr)
+		}
 	}
 }
 
 func main() {
-	var n, q int
-	fmt.Scanf("%d %d", &n, &q)
-	height := make([]int, n+1)
-	for i := 1; i <= n; i++ {
-		fmt.Scanf("%d", &height[i])
+	var m, n int
+	fmt.Scanf("%d %d", &n, &m)
+
+	values := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &values[i])
 	}
-	var t = NewTree(height)
-	for i := 0; i < q; i++ {
+
+	var t = New(values)
+	for i := 0; i < m; i++ {
 		var beg, end int
 		fmt.Scanf("%d %d", &beg, &end)
-		var min, max = t.MinMax(beg, end)
-		fmt.Printf("%d\n", max-min)
+		fmt.Println(t.Delta(beg-1, end-1))
 	}
 }
