6c8ca8e2c4a9fb073ac777c4d7998bdd1f63410e
spinlock <wnzheng@gmail.com>
2015-05-14 22:14:42 +0800

    poj, update init

--- poj/init.sh
+++ poj/init.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-id=$1; shift
+init() {
+    id=$1
+    rm -rf $id
+    mkdir -p $id && cd $id && wget -p -k http://poj.org/problem?id=$id
 
-if [ "x$id" == "x" ]; then
-    echo "invalid id"
-    exit 1
-fi
-
-rm -rf $id
-mkdir -p $id && cd $id && wget -p -k http://poj.org/problem?id=$id
-
-if [ $? -ne 0 ]; then
-    echo $?
-    exit 1
-fi
+    if [ $? -ne 0 ]; then
+        return $?
+    fi
 
 cat << EOF > Makefile
 all:
@@ -23,9 +17,27 @@ cc:
 	clang++ -O2 code.cc -o /tmp/pojcode.out && cat input | /tmp/pojcode.out
 EOF
 
-mv poj.org $id && cd $id && mv problem*$id ${id}.html
+    mv poj.org $id && cd $id && mv problem*$id ${id}.html
 
-if [ $? -ne 0 ]; then
-    echo $?
-    exit 1
-fi
+    if [ $? -ne 0 ]; then
+        return $?
+    fi
+
+    cd ../../
+    return 0
+}
+
+while true; do
+    id=$1; shift
+
+    if [ "x$id" == "x" ]; then
+        exit 0
+    fi
+
+    init $id
+    if [ $? -ne 0 ]; then
+        printf "\e[1;31m[ERROR] %s %d\n\e[0m" $id $?
+    else
+        printf "\e[1;32m[OK] %s\n\e[0m" $id
+    fi
+done
